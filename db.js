0a3e749ad3c6b61b9e4b77491e7648c5584ad4a0
Dhruv Bavadiya <dhruvbavadiya99@gmail.com>
2023-09-18 21:36:44 +0530

    connected to mongodb Atlas

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({path : "F:/dhruv/webd/practice/SIH/Backend/config/config.env"})
+
+const mongoURI = process.env.DB_URL
 const mongoconnect = () => {
     mongoose.connect(
-        process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).
+        mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).
         then((data) => {
         console.log(`${data.connection.host}`);
         })
